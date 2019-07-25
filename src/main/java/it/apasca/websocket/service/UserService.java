db44971b9f1d267b4bda74c025a5af6033036376
Bruno Conetta <b.conetta@almaviva.it>
2019-07-25 10:07:42 +0200

    user service

--- src/main/java/it/apasca/websocket/service/UserService.java
+++ src/main/java/it/apasca/websocket/service/UserService.java
@@ -4,6 +4,9 @@
 package it.apasca.websocket.service;
 
 import it.apasca.websocket.dto.UserDto;
+import it.apasca.websocket.model.User;
+
+import java.util.List;
 
 /**
  * @author a.pasca
@@ -12,5 +15,10 @@ import it.apasca.websocket.dto.UserDto;
 public interface UserService {
 	
 	public String registraUtente(UserDto user) throws Exception;
+	User createUser(User user);
+
+	void deleteUser(String roomId);
+
+	List<User> getUsers(User user);
 
 }
--- src/main/java/it/apasca/websocket/service/UserServiceImpl.java
+++ src/main/java/it/apasca/websocket/service/UserServiceImpl.java
@@ -4,10 +4,12 @@
 package it.apasca.websocket.service;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -57,4 +59,21 @@ public class UserServiceImpl implements UserService {
 		}
 	}
 
+	@Override
+	public User createUser(User user) {
+		User userToSave = new User();
+		BeanUtils.copyProperties(user , userToSave , "id");
+		User userSaved = userDao.save(userToSave);
+		return userSaved;
+	}
+
+	@Override
+	public void deleteUser(String userID) {
+		userDao.deleteById(userID);
+	}
+
+	@Override
+	public List<User> getUsers(User user) {
+		return userDao.findAll(Example.of(user));
+	}
 }
